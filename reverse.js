890d67e3445e8c3c8d3c5836ed6669e28f85345c
Yash Bhamania <100078597+Yash-Bhamania@users.noreply.github.com>
2025-06-21 16:17:46 +0000

    THis is the revser of an array

---
+++ reverse.js
@@ -0,0 +1,37 @@
+// ************************************** METHOD FIRST *********************************
+
+
+// let arr = ['i','n' ,'d'  ,'i' ,'a']
+
+// arr.reverse()
+
+// console.log(arr);
+
+// ************************************** METHOD SECOND *********************************
+
+// let arr = [1,2,4,5,6,7]
+
+// let reverseArr =[]
+
+// for(let i = arr.length -1; i>=0; i--){
+//     reverseArr.push(arr[i])
+// }
+
+// console.log(reverseArr);
+
+
+
+
+// ************************************** METHOD THIRD *********************************
+
+
+let arr = [1, 2, 4, 5, 6, 7]
+
+let reverseArr = []
+
+for (i = 0; i < arr.length; i++) {
+    reverseArr.unshift(arr[i])
+
+}
+
+console.log(reverseArr);
